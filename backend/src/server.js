d78e9af2698811df99b649cdd7108c737c36572f
Slash-495 <jainarush423@gmail.com>
2025-07-25 08:47:26 +0530

    Force remove backend node_modules from tracking

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,10 +7,13 @@ import authRoutes from "./routes/auth.js"
 import userRoutes from "./routes/user.js"
 import chatRoutes from "./routes/chat.js"
 import { connectDB } from "./lib/db.js";
+import path from "path";
 
 
 const app = express();
 const PORT = process.env.PORT
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -21,6 +24,14 @@ app.use(cors({
 app.use("/api/auth",authRoutes)
 app.use("/api/users",userRoutes)
 app.use("/api/chat",chatRoutes)
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    })
+}
+
 app.listen(PORT,()=>{
     console.log(`Server is running on Port ${PORT}`);
     connectDB();
